fd4e8f540eebb2ab27eac24cd7325f4ae1b1068c
しゅーたす <shutas@umich.edu>
2018-07-19 07:54:15 +0900

    MOD: Implemented BTree locate Method

--- btree/btree.py
+++ btree/btree.py
@@ -31,15 +31,58 @@ class BTree:
 
     def _insert(self, new_node, current_node):
         """Helper function to insert BNode to BTree. DFS."""
+        # If new_node is a child of current_node, add new_node
         if current_node == new_node.parent:
             current_node.children.append(new_node)
             new_node.parent = current_node
-            return
-        if current_node.children:
+            return "Inserted"
+
+        # Check if any child nodes are parent of new_node
+        elif current_node.children:
             for child in current_node.children:
-                self._insert(new_node, child)
+                result = self._insert(new_node, child)
+                if result:
+                    return "Found"
+
+        # Insert failed at this stage
+        return None
 
 
     def insert(self, new_node):
         """Insert BNode to tree. Print error if node cannot be inserted."""
-        self._insert(new_node, self.root)
+        # Insert BNode
+        inserted = self._insert(new_node, self.root)
+
+        # BNode cannot be inserted
+        if not inserted:
+            print("Error: BNode not inserted")
+
+    
+    def _locate(self, label_str, current_node):
+        """Helper function to locate BNode in BTree. DFS."""
+        # If curr_node has label_str, print parents' labels until root
+        if current_node.label == label_str:
+            while current_node != self.root:
+                print current_node.label + " >> ",
+                current_node = current_node.parent
+            print self.root.label
+            return "Found"
+
+        # Check if any child nodes have the label_str as its label
+        elif current_node.children:
+            for child in current_node.children:
+                result = self._locate(label_str, child)
+                if result:
+                    return "Found"
+
+        # Locate failed at this stage
+        return None
+
+    def locate(self, label_str):
+        """Locate label """
+        # Locate BNode
+        located = self._locate(label_str, self.root)
+        
+        # BNode not found
+        if not located:
+            print "Error: BNode not found"
